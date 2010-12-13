b3e53da99ab7671fce58d629d16389f8ab097690
fame lab <famelab@fbsdopti.mshome.net>
2010-12-13 20:48:03 +0000

    updated all modules

---
+++ sites/all/themes/famelab/global-footer.tpl.php
@@ -0,0 +1,21 @@
+
+
+  <div id="footer" class="prefix-1 suffix-1">
+    <?php if ($footer): ?>
+      <div id="footer-region" class="region grid-14 clear-block">
+        <?php print $footer; ?>
+      </div>
+    <?php endif; ?>
+
+    <?php if ($footer_message): ?>
+      <div id="footer-message" class="grid-14">
+        <?php print $footer_message; ?>
+      </div>
+    <?php endif; ?>
+  </div>
+
+
+  </div>
+  <?php print $closure; ?>
+</body>
+</html>
---
+++ sites/all/themes/famelab/page.tpl.php
@@ -0,0 +1,41 @@
+<?php
+// $Id: page.tpl.php,v 1.1.2.1 2009/02/24 15:34:45 dvessel Exp $
+include('global-header.tpl.php');
+
+?>
+
+<div id="main" class="column <?php print ns('grid-16', $left, 4, $right, 3) . ' ' . ns('push-4', !$left, 4); ?>">
+      <?php print $breadcrumb; ?>
+      
+      <?php if ($tabs): ?>
+        <div class="tabs"><?php print $tabs; ?></div>
+      <?php endif; ?>
+      
+      
+
+      <div id="main-content" class="region clear-block">
+          <?php if ($title): ?>
+            <h1 class="title" id="page-title"><?php print $title; ?></h1>
+          <?php endif; ?>
+          <?php print $help; ?>
+          <?php print $content; ?>
+      </div>
+
+      <?php #print $feed_icons; ?>
+</div>
+
+  <?php if ($left): ?>
+    <div id="sidebar-left" class="column sidebar region grid-4 <?php print ns('pull-12', $right, 3); ?>">
+      <?php print $left; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if ($right): ?>
+    <div id="sidebar-right" class="column sidebar region grid-3">
+      <?php print $right; ?>
+    </div>
+  <?php endif; ?>
+
+<?php
+include('global-footer.tpl.php');
+?>
