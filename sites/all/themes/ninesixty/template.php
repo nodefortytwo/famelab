adb9b97fcfb73218c3fd044517b9c5e52825261a
Rick Burgess <git@rickburgess.me>
2013-01-02 12:20:01 +0000

    logo path

--- sites/all/themes/ninesixty/template.php
+++ sites/all/themes/ninesixty/template.php
@@ -1,31 +1,35 @@
 <?php
 // $Id: template.php,v 1.1.2.3 2009/07/18 17:48:55 dvessel Exp $
 
-
 /**
  * Preprocessor for page.tpl.php template file.
  */
 function ninesixty_preprocess_page(&$vars, $hook) {
-    
-  if (strpos($vars['body_classes'], ' uk ')){
-      $uk = true;
-  }
- 
-  // For easy printing of variables.
-  $vars['logo_img']         = $vars['logo'] ? theme('image', substr($vars['logo'], strlen(base_path())), t('Home'), t('Home')) : '';
-  if ($uk){
-    $vars['linked_logo_img']  = $vars['logo_img'] ? l($vars['logo_img'], 'uk', array('attributes' => array('rel' => 'home'), 'title' => t('Home'), 'html' => TRUE)) : '';
-    $vars['linked_site_name'] = $vars['site_name'] ? l($vars['site_name'], 'uk', array('attributes' => array('rel' => 'home'), 'title' => t('Home'))) : '';
-  }else{
-    $vars['linked_logo_img']  = $vars['logo_img'] ? l($vars['logo_img'], '<front>', array('attributes' => array('rel' => 'home'), 'title' => t('Home'), 'html' => TRUE)) : '';
-    $vars['linked_site_name'] = $vars['site_name'] ? l($vars['site_name'], '<front>', array('attributes' => array('rel' => 'home'), 'title' => t('Home'))) : '';
-  }
-  $vars['main_menu_links']      = theme('links', $vars['primary_links'], array('class' => 'links main-menu'));
-  $vars['secondary_menu_links'] = theme('links', $vars['secondary_links'], array('class' => 'links secondary-menu'));
 
-  // Make sure framework styles are placed above all others.
-  $vars['css_alt'] = ninesixty_css_reorder($vars['css']);
-  $vars['styles'] = drupal_get_css($vars['css_alt']);
+	if (strpos($vars['body_classes'], ' uk ')) {
+		$uk = true;
+	} else if (strpos($vars['body_classes'], ' academy ')) {
+		$academy = true;
+	}
+
+	// For easy printing of variables.
+	$vars['logo_img'] = $vars['logo'] ? theme('image', substr($vars['logo'], strlen(base_path())), t('Home'), t('Home')) : '';
+	if ($uk) {
+		$vars['linked_logo_img'] = $vars['logo_img'] ? l($vars['logo_img'], 'uk', array('attributes' => array('rel' => 'home'), 'title' => t('Home'), 'html' => TRUE)) : '';
+		$vars['linked_site_name'] = $vars['site_name'] ? l($vars['site_name'], 'uk', array('attributes' => array('rel' => 'home'), 'title' => t('Home'))) : '';
+	} else if ($academy) {
+		$vars['linked_logo_img'] = $vars['logo_img'] ? l($vars['logo_img'], 'academy', array('attributes' => array('rel' => 'home'), 'title' => t('Home'), 'html' => TRUE)) : '';
+		$vars['linked_site_name'] = $vars['site_name'] ? l($vars['site_name'], 'academy', array('attributes' => array('rel' => 'home'), 'title' => t('Home'))) : '';
+	} else {
+		$vars['linked_logo_img'] = $vars['logo_img'] ? l($vars['logo_img'], '<front>', array('attributes' => array('rel' => 'home'), 'title' => t('Home'), 'html' => TRUE)) : '';
+		$vars['linked_site_name'] = $vars['site_name'] ? l($vars['site_name'], '<front>', array('attributes' => array('rel' => 'home'), 'title' => t('Home'))) : '';
+	}
+	$vars['main_menu_links'] = theme('links', $vars['primary_links'], array('class' => 'links main-menu'));
+	$vars['secondary_menu_links'] = theme('links', $vars['secondary_links'], array('class' => 'links secondary-menu'));
+
+	// Make sure framework styles are placed above all others.
+	$vars['css_alt'] = ninesixty_css_reorder($vars['css']);
+	$vars['styles'] = drupal_get_css($vars['css_alt']);
 }
 
 /**
@@ -42,31 +46,30 @@ function ninesixty_preprocess_page(&$vars, $hook) {
  * the default class. See the README.txt file.
  */
 function ns() {
-  $args = func_get_args();
-  $default = array_shift($args);
-  // Get the type of class, i.e., 'grid', 'pull', 'push', etc.
-  // Also get the default unit for the type to be procesed and returned.
-  list($type, $return_unit) = explode('-', $default);
+	$args = func_get_args();
+	$default = array_shift($args);
+	// Get the type of class, i.e., 'grid', 'pull', 'push', etc.
+	// Also get the default unit for the type to be procesed and returned.
+	list($type, $return_unit) = explode('-', $default);
 
-  // Process the conditions.
-  $flip_states = array('var' => 'int', 'int' => 'var');
-  $state = 'var';
-  foreach ($args as $arg) {
-    if ($state == 'var') {
-      $var_state = !empty($arg);
-    }
-    elseif ($var_state) {
-      $return_unit = $return_unit - $arg;
-    }
-    $state = $flip_states[$state];
-  }
+	// Process the conditions.
+	$flip_states = array('var' => 'int', 'int' => 'var');
+	$state = 'var';
+	foreach ($args as $arg) {
+		if ($state == 'var') {
+			$var_state = !empty($arg);
+		} elseif ($var_state) {
+			$return_unit = $return_unit - $arg;
+		}
+		$state = $flip_states[$state];
+	}
 
-  $output = '';
-  // Anything below a value of 1 is not needed.
-  if ($return_unit > 0) {
-    $output = $type . '-' . $return_unit;
-  }
-  return $output;
+	$output = '';
+	// Anything below a value of 1 is not needed.
+	if ($return_unit > 0) {
+		$output = $type . '-' . $return_unit;
+	}
+	return $output;
 }
 
 /**
@@ -78,42 +81,41 @@ function ns() {
  * weighted styles.
  */
 function ninesixty_css_reorder($css) {
-  global $theme_info, $base_theme_info;
+	global $theme_info, $base_theme_info;
 
-  // Dig into the framework .info data.
-  $framework = !empty($base_theme_info) ? $base_theme_info[0]->info : $theme_info->info;
+	// Dig into the framework .info data.
+	$framework = !empty($base_theme_info) ? $base_theme_info[0] -> info : $theme_info -> info;
 
-  // Pull framework styles from the themes .info file and place them above all stylesheets.
-  if (isset($framework['stylesheets'])) {
-    foreach ($framework['stylesheets'] as $media => $styles_from_960) {
-      // Setup framework group.
-      if (isset($css[$media])) {
-        $css[$media] = array_merge(array('framework' => array()), $css[$media]);
-      }
-      else {
-        $css[$media]['framework'] = array();
-      }
-      foreach ($styles_from_960 as $style_from_960) {
-        // Force framework styles to come first.
-        if (strpos($style_from_960, 'framework') !== FALSE) {
-          $framework_shift = $style_from_960;
-          $remove_styles = array($style_from_960);
-          // Handle styles that may be overridden from sub-themes.
-          foreach ($css[$media]['theme'] as $style_from_var => $preprocess) {
-            if ($style_from_960 != $style_from_var && basename($style_from_960) == basename($style_from_var)) {
-              $framework_shift = $style_from_var;
-              $remove_styles[] = $style_from_var;
-              break;
-            }
-          }
-          $css[$media]['framework'][$framework_shift] = TRUE;
-          foreach ($remove_styles as $remove_style) {
-            unset($css[$media]['theme'][$remove_style]);
-          }
-        }
-      }
-    }
-  }
+	// Pull framework styles from the themes .info file and place them above all stylesheets.
+	if (isset($framework['stylesheets'])) {
+		foreach ($framework['stylesheets'] as $media => $styles_from_960) {
+			// Setup framework group.
+			if (isset($css[$media])) {
+				$css[$media] = array_merge(array('framework' => array()), $css[$media]);
+			} else {
+				$css[$media]['framework'] = array();
+			}
+			foreach ($styles_from_960 as $style_from_960) {
+				// Force framework styles to come first.
+				if (strpos($style_from_960, 'framework') !== FALSE) {
+					$framework_shift = $style_from_960;
+					$remove_styles = array($style_from_960);
+					// Handle styles that may be overridden from sub-themes.
+					foreach ($css[$media]['theme'] as $style_from_var => $preprocess) {
+						if ($style_from_960 != $style_from_var && basename($style_from_960) == basename($style_from_var)) {
+							$framework_shift = $style_from_var;
+							$remove_styles[] = $style_from_var;
+							break;
+						}
+					}
+					$css[$media]['framework'][$framework_shift] = TRUE;
+					foreach ($remove_styles as $remove_style) {
+						unset($css[$media]['theme'][$remove_style]);
+					}
+				}
+			}
+		}
+	}
 
-  return $css;
+	return $css;
 }
