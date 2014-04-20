f4fb13e319d23d6ae38dd710ae91a33be53c49ab
Rick Burgess <nodefortytwo@gmail.com>
2014-04-20 16:22:33 +0100

    white bg

--- live-stream/index.php
+++ live-stream/index.php
@@ -299,7 +299,6 @@
 						</div>
 					</div>
 					<div id="block-boxes-twitter" class="block block-boxes">
-						<h2>Twitter</h2>
 						<div class="content">
 							<div id='boxes-box-twitter' class='boxes-box'>
 								<div class="boxes-box-content">
@@ -309,12 +308,6 @@
 							</div>
 						</div>
 					</div>
-
-
-					<a class="twitter-timeline" href="https://twitter.com/search?q=famelab" data-widget-id="330804112058880001">Tweets about "famelab"</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-
-
 				</div>
 			</div>
 		</div></div>
