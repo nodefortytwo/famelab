ea5475ef8a79c9f0f40f67511886c9711aafd002
Rick Burgess <nodefortytwo@gmail.com>
2014-04-20 16:20:03 +0100

    white bg

--- live-stream/index.php
+++ live-stream/index.php
@@ -298,6 +298,23 @@
 							</div>
 						</div>
 					</div>
+					<div id="block-boxes-twitter" class="block block-boxes">
+						<h2>Twitter</h2>
+						<div class="content">
+							<div id='boxes-box-twitter' class='boxes-box'>
+								<div class="boxes-box-content">
+									<a class="twitter-timeline" href="https://twitter.com/search?q=famelab" data-widget-id="330804112058880001">Tweets about "famelab"</a>
+									<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+								</div>
+							</div>
+						</div>
+					</div>
+
+
+					<a class="twitter-timeline" href="https://twitter.com/search?q=famelab" data-widget-id="330804112058880001">Tweets about "famelab"</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+
 				</div>
 			</div>
 		</div></div>
